715d6e9c7c377e33b62c6b285903f520c4d55a5d
stepa1000 <39490441+stepa1000@users.noreply.github.com>
2026-06-16 08:51:18 +0600

    flaking and build&run

--- src/AdjProfunctor/BuildRun.hs
+++ src/AdjProfunctor/BuildRun.hs
@@ -4,6 +4,8 @@ import Data.Profunctor
 import Data.Profunctor.Monad
 import Data.Prpfunctor.Adjunction
 
+import Control.Category.Free
+
 buildCoPasTamSum :: 
    ( ProfunctorComonad w
    , Profunctor a
@@ -46,7 +48,7 @@ runPasTamSum :: ( ProfunctorMonad m
    , Profunctor r
    , Choice r) => 
    (forall x y. a x y -> r x y) ->
-   ProMAdjointT CopastroSum CotambaraSum m a :-> 
+   ProMAdjointT PastroSum TambaraSum m a :-> 
    m r
 runPasTamSum f (ProMAdjointT pma) = promap (\ (PastroSum fy a fx) -> dimap fx fy (left' $ f a) ) $ proextract pma
 
@@ -69,6 +71,105 @@ runEnvClo :: ( ProfunctorMonad m
    m r
 runEnvClo f (ProMAdjointT pma) = promap (\ (Environment fy a fx) -> dimap fx fy (closed $ f a) ) $ proextract pma
 
+buildCoPasTam ::  
+   ( ProfunctorComonad w
+   , Profunctor a
+   , Profunctor b
+   , Costrang a) =>
+   (a :-> b) ->
+   w a :->
+   ProWAdjointT Copastro Cotambara w b
+buildCoPasTam f wa = ProWAdjointT $ proreturn $ promap (\a-> cotambara f a) wa
 
+runCoPasTam :: ( ProfunctorMonad m
+   , Profunctor a
+   , Profunctor r
+   , Costrong r) => 
+   (forall x y. a x y -> r x y) ->
+   ProMAdjointT Copastro Cotambara m a :-> 
+   m r
+runCoPasTam f (ProMAdjointT pma) = promap (\ (Copastro a) -> f a) $ proextract pma
+
+buildPasTam ::  
+   ( ProfunctorComonad w
+   , Profunctor a
+   , Profunctor b
+   , Strang a) =>
+   (a :-> b) ->
+   w a :->
+   ProWAdjointT Pastro Tambara w b
+buildPasTam f wa = ProWAdjointT $ proreturn $ promap (\a-> tambara f a) wa
+
+runPasTam :: ( ProfunctorMonad m
+   , Profunctor a
+   , Profunctor r
+   , Strong r) => 
+   (forall x y. a x y -> r x y) ->
+   ProMAdjointT Pastro Tambara m a :-> 
+   m r
+runPasTam f (ProMAdjointT pma) = promap (\ (Pastro fy a fx) -> dimap fx fy (first' $ f a) ) $ proextract pma
+
+buildCatRif ::  
+   ( ProfunctorComonad w
+   , Profunctor a
+   , Category p) =>
+   (p :-> a) ->
+   w b :->
+   ProWAdjointT (Procompose p) (Rift p) w a
+buildCatRif f wa = ProWAdjointT $ proreturn $ promap (\a-> Rift f) wa
+
+runCatRif :: ( ProfunctorMonad m
+   , Profunctor a
+   , Profunctor r
+   , Category p) => 
+   (forall x y. a x y -> p x y) ->
+   ProMAdjointT (Procompose p) (Rift p) m a :-> 
+   m p
+runCatRif f (ProMAdjointT pma) = promap (\ (Procompose fy fx) -> (fy . f fx) ) fx) $ proextract pma
+
+data ProAdjW p w a x y
+   = PAWTCPTS (ProWAdjointT CopastroSum CotambaraSum w a x y)
+   | PAWTPTS (ProWAdjointT PastroSum TambaraSum w a x y)
+   | PAWTEC (ProWAdjointT Environment Closure w a x y)
+   | PAWTCPT (ProWAdjointT Copastro Cotambara w a x y) 
+   | PAWTPT (ProWAdjointT Pastro Tambara w a x y)
+   | PAWTCR (ProWAdjointT (Procompose p) (Rift p) w a x y)
+   
+type CatProAdjW w a x y = Queue (ProAdjW (CatProAdjW w a) w a)
+
+data ProAdjM p m a x y
+   = PAMTCPTS (ProMAdjointT CopastroSum CotambaraSum m a x y)
+   | PAMTPTS (ProMAdjointT PastroSum TambaraSum m a x y)
+   | PAMTEC (ProMAdjointT Environment Closure m a x y)
+   | PAMTCPT (ProMAdjointT Copastro Cotambara m a x y) 
+   | PAMTPT (ProMAdjointT Pastro Tambara m a x y)
+   | PAMTCR (ProMAdjointT (Procompose p) (Rift p) m a x y)
+
+type CatProAdjM m a x y = Queue (ProAdjM (CatProAdjM m a) m a)
+
+{-
+data ProAdj (a :: ProAdj) -- p
+   = CoPasTamSun
+   | PasTamSum
+   | EnvCLo
+   | CoPasTam
+   | PasTam
+   | CatRif (Peoxy (a :: ProAdj)) -- (Proxy p) -- (Proxy (a :: ProAdj))
+
+data ProBuild p (a :: ProAdj p) w a where
+   ProCat :: 
+      ProWAdjointT 
+         (Procompose 
+	    (ProBuild p (a :: ProAdj a2) w)) 
+	 (Rift 
+	    (ProBuild p (a :: ProAdj a2) w)) 
+	 (ProWAdjointT 
+	    (Procompose 
+	       (ProBuild p (a :: ProAdj a2) w)) 
+	    (Rift 
+	       (ProBuild p (a :: ProAdj a2) w)) 
+	    ) a -> 
+	 ProBuild p (CatRif a) w a
+-}
 
 
